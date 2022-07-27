9656d74878691bc66cb418c05ceb71c5a731b4df
David <czdavid93@gmail.com>
2022-07-27 15:40:02 +0200

    working on add and remove answers

--- src/components/login/dashboard/CreateQuestionForm.jsx
+++ src/components/login/dashboard/CreateQuestionForm.jsx
@@ -3,38 +3,53 @@ import { useState } from 'react';
 function CreateQuestionsForm() {
   const [questionList, setQuestionList] = useState([
     {
-      question: 'first question',
-      answerList: [{ answer: 'first answer' }, { answer: 'second answer' }],
-    },
-    // {
-    //   question: 'second question',
-    //   answerList: [{ answer: '3rd answer' }, { answer: '4th answer' }],
-    // },
+      question: '1 question',
+      answerList: [{ answer: '1 answer' }],
+    }
   ]);
 
-  console.log(questionList);
-
   const handleSubmit = () => {};
-  const removeAnswer = () => {};
   const handleAnswerChange = () => {};
 
-  const addQuestion = (event) => {
-    event.preventDefault();
-    setQuestionList([...questionList, { question: '' }]);
-  };
+    const removeAnswer = (event, index, index2) => {
+        event.preventDefault()
+    
+        let copyQuestionList = [...questionList]
+        let questionAtIndex = copyQuestionList[index];
+        let theAnswers = questionAtIndex.answerList;
 
-  const removeQuestion = (event, index) => {
-    let list = [...questionList];
-    list.splice(index, 1);
-    setQuestionList(list);
-  };
+        theAnswers.splice(index2, 1);
+        setQuestionList(copyQuestionList);
+    };
 
-  const handleQuestionChange = (e, index) => {
-    const { name, value } = e.target;
-    let list = [...questionList];
-    list[index][name] = value;
-    setQuestionList(list);
-  };
+    const addQuestion = (event) => {
+        event.preventDefault();
+        setQuestionList([...questionList, { 
+        question: '',
+        answerList: [{ answer: '' }],
+        }]);
+    };
+
+    const removeQuestion = (event, index) => {
+        event.preventDefault()
+        let list = [...questionList];
+        list.splice(index, 1);
+        setQuestionList(list);
+    };
+
+    const handleQuestionChange = (e, index) => {
+        const { name, value } = e.target;
+        let list = [...questionList];
+        list[index][name] = value;
+        setQuestionList(list);
+    };
+
+    const addAnswer = (e, index) => {
+        e.preventDefault();
+        let list = [...questionList];
+        list[index].answerList.push({ answer: '' });
+        setQuestionList(list);
+    }
 
   return (
     <form className='question-form' onSubmit={handleSubmit}>
@@ -49,26 +64,30 @@ function CreateQuestionsForm() {
             value={singleQuestion.question}
             onChange={(e) => handleQuestionChange(e, index)}
           ></input>
-          <button onClick={() => removeQuestion(index)}>remove question</button>
+          <button onClick={(e) => removeQuestion(e, index)}>remove question</button>
 
           <div>
-            {singleQuestion.answerList.map((answer, index) => (
-              <div key={index}>
+            {singleQuestion.answerList.map((singleAnswer, index2) => (
+              <div key={index2}>
                 <input
                   className='answer-form-input'
                   type='text'
                   label='answer'
                   name='answer'
                   placeholder='answer'
-                  value={answer.answer}
-                  onChange={(e) => handleAnswerChange(e, index)}
+                  value={singleAnswer.answer}
+                  onChange={(e) => handleAnswerChange(e, index2)}
                 ></input>
-                <button onClick={() => removeAnswer(index)}>
+                <button onClick={(event) => removeAnswer(event, index, index2)}>
                   remove answer
                 </button>
               </div>
             ))}
           </div>
+
+          <button id='add-new-answer-button' onClick={(e) => addAnswer(e,index)}>
+            add answer
+          </button>
         </div>
       ))}
 
