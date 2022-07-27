1d9c40377d80223f5a697953ccf6d0e37839ac08
David <czdavid93@gmail.com>
2022-07-27 18:10:36 +0200

    form styling, icons

--- src/components/login/dashboard/CreateQuestionForm.jsx
+++ src/components/login/dashboard/CreateQuestionForm.jsx
@@ -4,14 +4,21 @@ function CreateQuestionsForm() {
   const [questionList, setQuestionList] = useState([
     {
       question: '',
-      answerList: [{ answer: '' }],
+      answerList: [{ answer: '', isThisCorrect: false }],
     }
   ]);
 
   const handleSubmit = () => {};
 
+  const resetFields = (event) => {
+    console.log('resetting')
+    event.preventDefault()
+
+
+  }
+
   const handleAnswerChange = (e, index, index2) => {
-    e.preventDefault()
+        e.preventDefault()
         const { name, value } = e.target;
         
         let copyQuestionList = [...questionList];
@@ -35,10 +42,13 @@ function CreateQuestionsForm() {
 
     const addQuestion = (event) => {
         event.preventDefault();
-        setQuestionList([...questionList, { 
-        question: '',
-        answerList: [{ answer: '' }],
-        }]);
+        setQuestionList([
+          ...questionList,
+          {
+            question: '',
+            answerList: [{ answer: '', isThisCorrect: false }],
+          },
+        ]);
     };
 
     const removeQuestion = (event, index) => {
@@ -63,53 +73,110 @@ function CreateQuestionsForm() {
         setQuestionList(list);
     }
 
+    const handleCheckboxChange = (event, index, index2) => {
+        event.preventDefault();
+        const value = event.target.checked;
+
+        let copyQuestionList = [...questionList];
+        let questionAtIndex = copyQuestionList[index];
+        let theAnswers = questionAtIndex.answerList;
+
+        theAnswers[index2].isThisCorrect = value;
+        setQuestionList(copyQuestionList);
+    };
+
   return (
     <form className='question-form' onSubmit={handleSubmit}>
       {questionList.map((singleQuestion, index) => (
-        <div key={index}>
-          <input
-            className='question-form-input'
-            type='text'
-            label='question'
-            name='question'
-            placeholder='question'
-            value={singleQuestion.question}
-            onChange={(e) => handleQuestionChange(e, index)}
-          ></input>
-          <button onClick={(e) => removeQuestion(e, index)}>remove question</button>
+        <div key={index} className='question-panel'>
+          <p>Question #{index + 1}</p>
+          <div className='row-flex'>
+            <input
+              className='question-form-input'
+              type='text'
+              label='question'
+              name='question'
+              placeholder='question'
+              value={singleQuestion.question}
+              onChange={(e) => handleQuestionChange(e, index)}
+            ></input>
+            <button
+              className='button-reset'
+              onClick={(e) => removeQuestion(e, index)}
+            >
+              <span className='material-symbols-rounded'>delete</span>
+            </button>
+          </div>
 
           <div>
             {singleQuestion.answerList.map((singleAnswer, index2) => (
               <div key={index2}>
-                <input
-                  className='answer-form-input'
-                  type='text'
-                  label='answer'
-                  name='answer'
-                  placeholder='answer'
-                  value={singleAnswer.answer}
-                  onChange={(e) => handleAnswerChange(e, index, index2)}
-                ></input>
-                <button onClick={(event) => removeAnswer(event, index, index2)}>
-                  remove answer
-                </button>
+                <p>Answer #{index2 + 1}</p>
+                <div className='row-flex'>
+                  <input
+                    type='checkbox'
+                    value={singleAnswer.isThisCorrect}
+                    onChange={(e) => handleCheckboxChange(e, index, index2)}
+                  />
+
+                  <input
+                    className='answer-form-input'
+                    type='text'
+                    label='answer'
+                    name='answer'
+                    placeholder='answer'
+                    value={singleAnswer.answer}
+                    onChange={(e) => handleAnswerChange(e, index, index2)}
+                  ></input>
+
+                  <button
+                    className='button-reset'
+                    onClick={(event) => removeAnswer(event, index, index2)}
+                  >
+                    <span className='material-symbols-rounded'>delete</span>
+                  </button>
+                </div>
               </div>
             ))}
           </div>
 
-          <button id='add-new-answer-button' onClick={(e) => addAnswer(e,index)}>
-            add answer
+          <button
+            className='button-reset'
+            id='add-new-answer-button'
+            onClick={(e) => addAnswer(e, index)}
+          >
+            <span className='material-symbols-outlined'>add</span>add answer
           </button>
         </div>
       ))}
+      <div className='spaceAround'>
+        <button
+          className='button-reset'
+          id='add-new-question-button'
+          onClick={addQuestion}
+        >
+          <span className='material-symbols-outlined'>add</span>
+          add question
+        </button>
 
-      <button id='add-new-question-button' onClick={addQuestion}>
-        add question
-      </button>
+        <button
+          className='button-reset'
+          id='clear-button'
+          onClick={(e) => resetFields(e)}
+        >
+          <span className='material-symbols-outlined'>device_reset</span>
+          clear all fields
+        </button>
 
-      <button id='question-submit-button' type='submit'>
-        submit
-      </button>
+        <button
+          id='question-submit-button'
+          type='submit'
+          className='button-reset'
+        >
+          <span className='material-symbols-outlined'>done</span>
+          submit
+        </button>
+      </div>
     </form>
   );
 }
